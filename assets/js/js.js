46f177c30453cb7429c111001764bd430622c298
offcorse <57239657+offcorse@users.noreply.github.com>
2020-08-27 14:37:47 -0600

    Add files via upload

---
+++ assets/js/js.js
@@ -0,0 +1,51 @@
+function ExpandDivOne() {
+  var x = document.getElementById("divOne");
+    var x2 = document.getElementById("btn1")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    x2.innerHTML = "Hide Section";
+  } else {
+    x.style.display = "none";
+      x2.innerHTML = "Show Section";
+      
+  }
+}
+
+function ExpandDivTwo() {
+  var x = document.getElementById("divTwo");
+    var x2 = document.getElementById("btn2")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    x2.innerHTML = "Hide Section";
+  } else {
+    x.style.display = "none";
+      x2.innerHTML = "Show Section";
+      
+  }
+}
+
+function ExpandDivThree() {
+  var x = document.getElementById("divThree");
+    var x2 = document.getElementById("btn3")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    x2.innerHTML = "Hide Section";
+  } else {
+    x.style.display = "none";
+      x2.innerHTML = "Show Section";
+      
+  }
+}
+
+function ExpandDivFour() {
+  var x = document.getElementById("divFour");
+    var x2 = document.getElementById("btn4")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    x2.innerHTML = "Hide Section";
+  } else {
+    x.style.display = "none";
+      x2.innerHTML = "Show Section";
+      
+  }
+}
